21d680474f12409d87f9415124f3a37c1fece341
Streit-C <114953493+Streit-C@users.noreply.github.com>
2025-05-19 20:13:36 -0700

    Update main.py
    Expanded main to include new functions.

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ Created on Sat Apr 26 12:18:03 2025
 
 import os
 from pathlib import Path
-from processing import process_nanoindentation_data, calculate_film_averages
-from display import plot_single_curve, plot_all_single_curves, plot_all_curves
+from processing import process_nanoindentation_data, calculate_film_averages, process_stress_strain_data
+from display import plot_single_curve, plot_all_single_curves, plot_all_curves, plot_all_curves_stress_strain
 
 def main():
     root_directory = r"D:\Data\Combi Ceramics\MoTaNbCrZr Combi\RT Wafer As-deposited"
-    processed_directory = os.path.abspath("Processed_Data")
+    processed_directory = os.path.abspath("Processed_Data Stress_Strain")
     
     process_nanoindentation_data(root_directory, remove_outliers_flag=True)
     
@@ -28,7 +28,13 @@ def main():
     
     plot_all_single_curves(root_directory, 'III', 'B', 7, plot_hardness=True, plot_modulus=True)
     
-    calculate_film_averages(processed_directory, output_file="film_mechanical_properties.csv", depth_range=(200, 500)) 
+    calculate_film_averages(processed_directory, output_file="film_mechanical_properties.csv", depth_range=(200, 500))
+    
+    process_stress_strain_data(root_directory, remove_outliers_flag=True, outlier_threshold=3.5, max_strain=18.0)
+    
+    plot_all_curves_stress_strain(processed_directory)
+    
+    plot_single_curve(processed_directory, 'IV', 'A', 7 , plot_hardness=False, plot_modulus=False, plot_stress=True)
 
 if __name__ == "__main__":
     main()
