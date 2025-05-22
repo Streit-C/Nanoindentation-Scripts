de0f809158fe6e04b534ce5001b5445c2a2147f5
Streit-C <114953493+Streit-C@users.noreply.github.com>
2025-05-22 14:25:12 -0700

    Update main.py
    Added cluster analysis and wafer plot generation.

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ Created on Sat Apr 26 12:18:03 2025
 """
 
 import os
+import plotly.io as pio
 from pathlib import Path
-from processing import process_nanoindentation_data, calculate_film_averages, process_stress_strain_data
-from display import plot_single_curve, plot_all_single_curves, plot_all_curves, plot_all_curves_stress_strain
+from processing import process_nanoindentation_data, calculate_film_averages, process_stress_strain_data, generate_elbow_plot, kmeans_clustering, feature_kmeans
+from display import plot_single_curve, plot_all_single_curves, plot_all_single_stress_strain_curves, plot_all_curves, plot_all_curves_stress_strain, plot_wafer_stress_strain, plot_wafer_stress_strain_interactive, plot_cluster_representatives
 
 def main():
+    pio.renderers.default = 'browser'  #Necessary command for generating images in browser using plotly
+    
     root_directory = r"D:\Data\Combi Ceramics\MoTaNbCrZr Combi\RT Wafer As-deposited"
-    processed_directory = os.path.abspath("Processed_Data Stress_Strain")
+    processed_directory = os.path.abspath("Processed_Data_Stress_Strain")
+    positions = "positions.csv"
+    cluster_file = "feature_clusters.csv"
     
     process_nanoindentation_data(root_directory, remove_outliers_flag=True)
     
@@ -30,11 +35,23 @@ def main():
     
     calculate_film_averages(processed_directory, output_file="film_mechanical_properties.csv", depth_range=(200, 500))
     
+    #Stress-strain curves
     process_stress_strain_data(root_directory, remove_outliers_flag=True, outlier_threshold=3.5, max_strain=18.0)
     
     plot_all_curves_stress_strain(processed_directory)
     
     plot_single_curve(processed_directory, 'IV', 'A', 7 , plot_hardness=False, plot_modulus=False, plot_stress=True)
+    plot_all_single_stress_strain_curves(root_directory, 'IV', 'A', 9, colormap='viridis')
+    
+    plot_wafer_stress_strain(processed_directory, positions, scale=0.072, region_min=0.05, region_max=0.95)
+    plot_wafer_stress_strain_interactive(processed_directory, positions, scale=0.065, region_min=0.05, region_max=0.95, use_colormap=True)
+    
+    #K-means cluster analysis
+    generate_elbow_plot(processed_directory)
+    kmeans_clustering(processed_directory, n_clusters=4)
+    
+    feature_kmeans(processed_directory, n_clusters=4, k_range=(1, 10), output_file="feature_clusters.csv", export_features_csv=None, show_elbow=True)
+    plot_cluster_representatives(processed_directory, cluster_file, figsize=(10, 14))
 
 if __name__ == "__main__":
     main()
